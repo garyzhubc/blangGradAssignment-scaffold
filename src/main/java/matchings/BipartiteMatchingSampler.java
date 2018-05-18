f05138ce8d62be6308577708ab359b1dbd2ccc5c
garyzhubc <zhupy@alumni.ubc.ca>
2018-05-18 02:18:43 -0700

    simplify procedure

--- src/main/java/matchings/BipartiteMatchingSampler.java
+++ src/main/java/matchings/BipartiteMatchingSampler.java
@@ -76,65 +76,90 @@ public class BipartiteMatchingSampler implements Sampler {
 	  int s;
 	  int t;
 	  
-	  // todo: simplify three cases to one
-	  if (k==n) {
-		  // empty matching
-//		  System.out.print("empty matching\n");
-		  i = rand.nextInt(n);
-		  j = rand.nextInt(n);
-		  conn.set(i,j);
-		  log_prob_otn = -Math.log(Math.pow(n,2)); // todo: check
-		  log_prob_nto = -Math.log(Math.pow(n-1,2)+1); // todo: check
-	  } else if (k==0) {
-		  // full matching
-//		  System.out.print("full matching\n");
-		  i = rand.nextInt(n);
-		  conn.set(i,-1);
-		  // both equals 1/n
-		  log_prob_otn = 0; // todo: check
-		  log_prob_nto = 0; // todo: check
-	  } else {
-		  // partial matching
-//		  System.out.print("partial matching\n");
-//		  System.out.printf("k=%d\n",k);
-//		  System.out.printf("m=%d\n",m);
-		  i = rand.nextInt(k*k+m);
-//		  System.out.printf("k^2+m=%d\n",k^2+m);
-//		  System.out.printf("i=%d\n",i);
-//		  System.out.printf("m-1=%d\n",m-1);
+//	  // todo: simplify three cases to one
+//	  if (k==n) {
+//		  // empty matching
+////		  System.out.print("empty matching\n");
+//		  i = rand.nextInt(n);
+//		  j = rand.nextInt(n);
+//		  conn.set(i,j);
+//		  log_prob_otn = -Math.log(Math.pow(n,2)); // todo: check
+//		  log_prob_nto = -Math.log(Math.pow(n-1,2)+1); // todo: check
+//	  } else if (k==0) {
+//		  // full matching
+////		  System.out.print("full matching\n");
+//		  i = rand.nextInt(n);
+//		  conn.set(i,-1);
+//		  // both equals 1/n
+//		  log_prob_otn = 0; // todo: check
+//		  log_prob_nto = 0; // todo: check
+//	  } else {
+//		  // partial matching
+////		  System.out.print("partial matching\n");
+////		  System.out.printf("k=%d\n",k);
+////		  System.out.printf("m=%d\n",m);
+//		  i = rand.nextInt(k*k+m);
+////		  System.out.printf("k^2+m=%d\n",k^2+m);
+////		  System.out.printf("i=%d\n",i);
+////		  System.out.printf("m-1=%d\n",m-1);
+//		  if (i<=m-1) {
+//			  // delete an edge
+//			  // need: indices that are not -1 
+////			  System.out.print("delete an edge\n");
+//			  j = unfr1.get(i);
+//			  conn.set(j,-1);
+//			  // total: k^2+m
+//			  log_prob_otn = -Math.log(Math.pow(k,2)+m); // todo: check
+//			  log_prob_nto = -Math.log(Math.pow(k-1,2)+m+1); // todo: check
+//		  } else {
+//			  // add an edge
+////			  System.out.print("add an edge\n");
+////			  System.out.printf("i=%d\n",i);
+//			  i = i-m;
+////			  System.out.printf("i_special=%d\n",i);
+////			  System.out.printf("k=%d\n",k);
+//			  s = i/k;
+//			  j = i%k;
+////			  System.out.printf("s=%d\n",s);
+////			  System.out.printf("j=%d\n",j);
+////			  System.out.print(fr1);
+////			  System.out.print("\n");
+////			  System.out.print(fr2);
+////			  System.out.print("\n");
+//			  l = fr1.get(s);
+//			  q = fr2.get(j);
+////			  System.out.printf("l=%d\n",l);
+////			  System.out.printf("q=%d\n",q);
+//			  conn.set(l,q);
+//			  log_prob_otn = -Math.log(Math.pow(k,2)+m); // todo: check
+//			  log_prob_nto = -Math.log(Math.pow(k+1,2)+m-1); // todo: check
+//		  }
+//	  }
+	  
+	  i = rand.nextInt(k*k+m);
+	  if (k!=0) {
 		  if (i<=m-1) {
-			  // delete an edge
-			  // need: indices that are not -1 
-//			  System.out.print("delete an edge\n");
 			  j = unfr1.get(i);
 			  conn.set(j,-1);
-			  // total: k^2+m
-			  log_prob_otn = -Math.log(Math.pow(k,2)+m); // todo: check
-			  log_prob_nto = -Math.log(Math.pow(k-1,2)+m+1); // todo: check
+			  log_prob_otn = -Math.log(Math.pow(k,2)+m);
+			  log_prob_nto = -Math.log(Math.pow(k-1,2)+m+1); 
 		  } else {
-			  // add an edge
-//			  System.out.print("add an edge\n");
-//			  System.out.printf("i=%d\n",i);
 			  i = i-m;
-//			  System.out.printf("i_special=%d\n",i);
-//			  System.out.printf("k=%d\n",k);
 			  s = i/k;
 			  j = i%k;
-//			  System.out.printf("s=%d\n",s);
-//			  System.out.printf("j=%d\n",j);
-//			  System.out.print(fr1);
-//			  System.out.print("\n");
-//			  System.out.print(fr2);
-//			  System.out.print("\n");
 			  l = fr1.get(s);
 			  q = fr2.get(j);
-//			  System.out.printf("l=%d\n",l);
-//			  System.out.printf("q=%d\n",q);
 			  conn.set(l,q);
-			  log_prob_otn = -Math.log(Math.pow(k,2)+m); // todo: check
-			  log_prob_nto = -Math.log(Math.pow(k+1,2)+m-1); // todo: check
+			  log_prob_otn = -Math.log(Math.pow(k,2)+m);
+			  log_prob_nto = -Math.log(Math.pow(k+1,2)+m-1); 
 		  }
+	  } else {
+		  i = rand.nextInt(n);
+		  conn.set(i,-1);
+		  log_prob_otn = 0; 
+		  log_prob_nto = 0; 
 	  }
+	  
 	  // accept or reject
 	  double log_prob_n = logDensity();
 //	  System.out.printf("log_prob_n=%f\n",log_prob_n);
