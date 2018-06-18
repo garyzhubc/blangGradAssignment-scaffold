d00f9113f0120ff15e709d73af50b9c03dbc157b
Peiyuan Zhu <zhupy@cedar5.cedar.computecanada.ca>
2018-06-18 15:00:57 -0700

    update

--- nextflow/permuted-clustering.sh
+++ nextflow/permuted-clustering.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-bouchar3
-#SBATCH --time=24:00:00
+#SBATCH --time=4:00:00
 #SBATCH --job-name=perm
 #SBATCH --mem=999999M
 #SBATCH --output=%x-%j.out
+module load java
 nextflow run -resume permuted-clustering.nf
